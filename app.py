905d8d9ad9d297546c284844ac5c88a0a5524dd1
teapetit <teapetit@gmail.com>
2026-02-16 14:55:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,9 @@ def api_paris():
     ]
 
     return jsonify(result)
-
+@app.route("/rapport")
+def mongraphique():
+    return render_template("graphique.html")
 
 
 # Ne rien mettre après ce commentaire
